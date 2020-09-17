3e4c9b548c2d42e26a3b2cd5464271665296ca7d
trebesh <50559259+trebesh@users.noreply.github.com>
2020-09-18 11:37:30 +1200

    Naieve Working
    No math involved.
    Each ant visits each set/node in a random order.

--- Ant.java
+++ Ant.java
@@ -1,47 +1,75 @@
-package com.company;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Ant {
 
-    ArrayList<int> Path = new ArrayList<int>[];
-    ArrayList<int> Pheromones = new ArrayList<int>[];
+    public ArrayList<Integer> Path = new ArrayList<Integer>();
+    public static ArrayList<Integer> Pheromones = new ArrayList<Integer>();
+    public boolean seenAll = false;
+
 //creates an ant object
     public Ant()
-    {
-
-    }
+    { }
 //adds the next step to the ants path
-    public static void AddToPath(int nextStep)
+    public void addToPath(int nextStep)
     {
-        Path.add(nextStep);
+        if(nextStep != -1) Path.add(nextStep);
     }
 //returns the ants current path
-    public static ArrayList<int> getPath()
+    public ArrayList<Integer> getPath()
     {
-        return path;
+        return Path;
     }
 //gets the next step for the ant
-    public static int getNextStep(Universe universe, ArrayList<int> pheromones)
+    public int getNextStep(ArrayList<ArrayList<Integer>> sets)//, ArrayList<Integer> pheromones)
     {
-        ArrayList<int> options = new ArrayList<int>[];
-        int nextStep = -1;
-        for(int i=0; i<universe.length(); i++)
-        {
-            int found = 0;
-            for(int j: Path)
-            {
-                if(i = j)
-                {
-                    found = 1;
-                }
+        if (!seenAll) {
+            int nextStep = -1;
+            ArrayList<Integer> options = new ArrayList<Integer>();
+
+            for (int i = 0; i < sets.size(); i++) {
+                if (!hasInPath(i)) options.add(i);
+                //System.out.println("Options size: " + options.size());
             }
-            if (found = 0)
-            {
-                options.add(i);
+
+            //        for (int i = 0; i < sets.size(); i++) {
+            //            int found = 0;
+            //            for (int j : Path) {
+            //                if (i == j) {
+            //                    found = 1;
+            //                }
+            //            }
+            //            if (found == 0) {
+            //                options.add(i);
+            //            }
+            //        }
+            try {
+                if (options.size() != 0) {
+                    Random rand = new Random();
+                    int randStep = rand.nextInt(options.size());
+                    //System.out.println("RandStep: " + randStep);
+                    nextStep = options.get(randStep);
+                } else seenAll = true;
+            } catch (Exception e) {
+                System.out.println();
+                System.out.println("ERROR in getNextStep");
+                System.out.println(e.toString());
             }
+            return nextStep;
         }
-        Random rand = new Random();
-        int randStep = rand.nextInt(options.length());
-        nextStep = options.get(randStep);
-        return nextStep;
+        else return -1;
+    }
+
+//Checks if a node/set is already in the ants path
+    public boolean hasInPath(int target){
+        for (int i: Path) {
+            if (i == target) return true;
+        }
+        return  false;
+    }
+
+//Print a string representation of the Ant
+    public String toString(){
+        return this.Path.toString();
     }
 }
--- SCP.java
+++ SCP.java
@@ -10,9 +10,17 @@ import java.util.ArrayList;
 
 public class SCP {
 
+    public static int colonySize = 0;
+    public static int generation = 0;
+    public static int generationSize = 0;
+
     public static String filename;
     public static BufferedReader reader;
-    public static ArrayList<Integer> universe;
+    public static ArrayList<Integer> universe = new ArrayList<Integer>();
+    public static ArrayList<Ant> colony = new ArrayList<Ant>();
+    public static ArrayList<Integer> set = new ArrayList<Integer>();
+    public static ArrayList<ArrayList<Integer>> sets = new ArrayList<ArrayList<Integer>>();
+    public static String[] inString;
 
     public static void main(String[] args) {
         try{
@@ -20,6 +28,14 @@ public class SCP {
 
             //Read in the file
             getProblem(filename);
+
+            //Create the first generation of Ants
+            createColony();
+
+            //Run generation till all ants have visited all nodes
+            runGeneration();
+
+            printColony();
         }catch (Exception e){
             System.out.println("Arguments not in correct format");
             System.out.println("Try: FileName.txt");
@@ -27,20 +43,90 @@ public class SCP {
 
     }
 
+// Reads in the problem Instance to be dealt with
     public static void getProblem(String filename){
+        String line = "";
         try {
             //Get the problem instance
             reader = new BufferedReader(new FileReader(filename));
-            String line = reader.readLine();
+            line = reader.readLine();
 
             //Assume the first line contains the Universe
-            universe = Integer.parseInt(line.split(","));
+            inString = line.split(",");
 
-            while (line != null){
-
+            for (int i = 0; i < inString.length; i++) {
+                universe.add(Integer.parseInt(inString[i]));
             }
+            System.out.println("Universe:" + universe);
+
+            //Gather in the sets
+            line = reader.readLine();
+            while (line != null){
+                inString = line.split(",");
+                for (int i = 0; i < inString.length; i++) {
+                    set.add(Integer.parseInt(inString[i]));
+                }
+                //System.out.println("Set: " + set);
+                sets.add(set);
+                set = new ArrayList<Integer>();
+                line = reader.readLine();
+            }
+            System.out.println("Sets: " + sets);
 
         }catch(FileNotFoundException e){System.out.println("File Not Found : " + filename);}
-        catch(Exception e){System.out.println("ERROR in getProblem");}
+        catch(Exception e){
+            System.out.println("ERROR in getProblem");
+            System.out.println("File Name: " + filename);
+            System.out.println("Universe: " + universe);
+            System.out.println("Line: " + line);
+            System.out.println("InString: " + inString.toString());
+            System.out.println();
+            System.out.println(e.toString());
+        }
+    }
+
+// Creates the colony and first generation of Ants
+    public static void createColony(){
+        try {
+            //Figure out how big the colony and each generation should be - relative to the number of sets in the search space
+            // Naieve - one ant for every set in sets, one generation
+            colonySize = sets.size();
+            generationSize = sets.size();
+
+            //Create the colony
+            while (colony.size() <= colonySize) {
+                colony.add(new Ant());
+            }
+
+
+            //Send out the first generation (gen 0) 1 step
+            for (Ant ant : colony) {
+                ant.addToPath(ant.getNextStep(sets));
+            }
+
+            //printColony();
+
+        }catch(Exception e){
+            System.out.println("ERROR in createColony");
+            System.out.println("Colony: " + colony);
+            System.out.println();
+            System.out.println(e.toString());
+        }
+    }
+
+// Runs a generation through till there are no places that each hasn't visited
+    public static void runGeneration(){
+        for (Ant ant: colony) {
+            while (ant.seenAll == false){
+                ant.addToPath(ant.getNextStep(sets));
+            }
+        }
+    }
+
+//Auxillary method to output the colony
+    public static void printColony(){
+        for (int i = 0; i < colony.size(); i++) {
+            System.out.println("Ant " + i  + " has path " + colony.get(i).toString());
+        }
     }
 }
