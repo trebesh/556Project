686aa6e47c11efbd5ebd35cdfb0099877e89c4bb
trebesh <50559259+trebesh@users.noreply.github.com>
2020-10-06 12:31:17 +1300

    Pheremones working actually this time

--- Ant.java
+++ Ant.java
@@ -1,42 +1,97 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Collections;
 
 public class Ant {
 
+    public ArrayList<Integer> seenNumbers = new ArrayList<Integer>();
     public ArrayList<Integer> Path = new ArrayList<Integer>();
     public static ArrayList<Integer> Pheromones = new ArrayList<Integer>();
     public boolean seenAll = false;
 
-//creates an ant object
-    public Ant()
-    { }
-//adds the next step to the ants path
+    //creates an ant object
+    public Ant(){    }
+    //adds the next step to the ants path
     public void addToPath(int nextStep)
     {
-        if(nextStep != -1) Path.add(nextStep);
+        if(nextStep != -1)
+        {
+            Path.add(nextStep);
+        }
     }
-//returns the ants current path
+    //returns the ants current path
     public ArrayList<Integer> getPath()
     {
         return Path;
     }
-//gets the next step for the ant
-    public int getNextStep(ArrayList<ArrayList<Integer>> sets)//, ArrayList<Integer> pheromones)
+    //gets the next step for the ant
+    public int getNextStep(ArrayList<ArrayList<Integer>> sets, ArrayList<Integer> pheremones)
     {
+        ArrayList<Integer> pathWeight = new ArrayList<Integer>();
+        ArrayList<Integer> ranges = new ArrayList<Integer>();
+        int range = 0;
+        Random rand = new Random();
+        int moveto;
+        int next = -1;
+
         if (!seenAll) {
             int nextStep = -1;
             ArrayList<Integer> options = new ArrayList<Integer>();
 
             for (int i = 0; i < sets.size(); i++) {
-                if (!hasInPath(i)) options.add(i);
+                if (!hasInPath(i)) {
+                    options.add(i);
+                    pathWeight.add(pheremones.get(i));
+                }
                 //System.out.println("Options size: " + options.size());
             }
+
             try {
                 if (options.size() != 0) {
-                    Random rand = new Random();
-                    int randStep = rand.nextInt(options.size());
-                    //System.out.println("RandStep: " + randStep);
-                    nextStep = options.get(randStep);
+                    ArrayList<Integer> pathDistance = new ArrayList<Integer>();
+                    pathDistance = getLength(options, sets);
+
+                    for (int i = 0; i<pathDistance.size(); i++)
+                    {
+                        if(pathDistance.get(i) == 0)
+                        {
+                            pathDistance.remove(i);
+                            options.remove(i);
+                            pathWeight.remove(i)
+;                        }
+                    }
+
+                    //Weight the paths
+                    //System.out.println("PathWeight: " + pathWeight);
+                    for (int i = 0; i < options.size(); i++){
+                        pathWeight.set(i, pathWeight.get(i) + (pathDistance.get(i) * 10));
+                    }
+                    //System.out.println("PathWeight: " + pathWeight);
+
+                    //Determine the probability ranges
+                    for (Integer i:pathWeight) {
+                        range += i;
+                        ranges.add(range);
+                    }
+
+                    moveto = rand.nextInt(range);
+
+                    for (int i = 0; i < ranges.size() && next == -1; i++){
+                        if(moveto < ranges.get(i)){
+                            next = i;
+                        }
+                    }
+
+                    //System.out.println("Range: " + range);
+
+                    nextStep = options.get(next);
+                    for(int i:sets.get(nextStep))
+                    {
+                        if(!(seenNumbers.contains(i)))
+                        {
+                            seenNumbers.add(i);
+                        }
+                    }
                 } else seenAll = true;
             } catch (Exception e) {
                 System.out.println();
@@ -48,7 +103,7 @@ public class Ant {
         else return -1;
     }
 
-//Checks if a node/set is already in the ants path
+    //Checks if a node/set is already in the ants path
     public boolean hasInPath(int target){
         for (int i: Path) {
             if (i == target) return true;
@@ -56,8 +111,60 @@ public class Ant {
         return  false;
     }
 
-//Print a string representation of the Ant
+    //Print a string representation of the Ant
     public String toString(){
-        return this.Path.toString();
+        return "Path: " + this.Path.toString();
+    }
+
+    public boolean seenAllNumbers(ArrayList<Integer> Universe, ArrayList<ArrayList<Integer>> sets)
+    {
+        ArrayList<Integer> seen = new ArrayList<Integer>();
+        for(int i: Path)
+        {
+            for(int j:sets.get(i))
+            {
+                if(!(seen.contains(j)))
+                {
+                    seen.add(j);
+                }
+            }
+        }
+        Collections.sort(seen);
+        if(seen.equals(Universe))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> getLength(ArrayList<Integer> options, ArrayList<ArrayList<Integer>> sets)
+    {
+        ArrayList<Integer> pathDistance = new ArrayList<Integer>();
+        for(int i = 0; i<options.size(); i++)
+        {
+            int score = 0;
+            for(int j:sets.get(options.get(i)))
+            {
+                if(!(seenNumbers.contains(j)))
+                {
+                    score+=1;
+                }
+            }
+            pathDistance.add(score);
+        }
+
+//        for(int i:pathDistance)
+//        {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println("");
+        return pathDistance;
+    }
+
+    public void startPositions(ArrayList<ArrayList<Integer>> sets)
+    {
+        Random rand = new Random();
+        int randStep = rand.nextInt(sets.size());
+        Path.add(randStep);
     }
 }
--- SCP.java
+++ SCP.java
@@ -1,18 +1,24 @@
 /*
   Class to produce solutions to the Set Cover Problem by implementing parellel Ant Colony Optimisation.
     ConnorFergusson_1299038_HannahTrebes_1306378
+
+    Command Line:
+    javac *.java && java SCP <TEST_FILE>.txt <COLONY_SIZE> <GENERATION_SIZE>
 */
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SCP {
 
     public static int colonySize = 0;
     public static int generation = 0;
     public static int generationSize = 0;
+    public static int numGenerations = 0;
+    public static int degrade = 1;
 
     public static String filename;
     public static BufferedReader reader;
@@ -21,10 +27,17 @@ public class SCP {
     public static ArrayList<Integer> set = new ArrayList<Integer>();
     public static ArrayList<ArrayList<Integer>> sets = new ArrayList<ArrayList<Integer>>();
     public static String[] inString;
+    public static ArrayList<Integer> bestPath;// = new ArrayList<Integer>();
+    public static ArrayList<Integer> pheremones = new ArrayList<Integer>();
+
 
     public static void main(String[] args) {
         try{
             filename = args[0];
+            colonySize = Integer.parseInt(args[1]);
+            generationSize = Integer.parseInt(args[2]);
+            if((colonySize % generationSize) != 0) throw new Exception("Generation size must be an integer factor of colony size.");
+            numGenerations = (colonySize/generationSize);
 
             //Read in the file
             getProblem(filename);
@@ -33,17 +46,65 @@ public class SCP {
             createColony();
 
             //Run generation till all ants have visited all nodes
-            runGeneration();
+            for (int i = 0; i < numGenerations; i++)runGeneration();
 
+            //Output results in the form of the colony
+            System.out.println();
+            System.out.println("Final Paths: ");
             printColony();
+
+            //Calculate final Results
+            int bi = 0;
+            int numBest = 0;
+            int worstsize = 0;
+            int numWorst = 0;
+            int averageLength = 0;
+            bestPath = colony.get(0).Path;
+            for (int i = 0; i < colonySize; i++) {
+                if(colony.get(i).Path.size() < bestPath.size()) {
+                    bestPath = colony.get(i).Path;
+                    bi = i;
+                }
+                if(colony.get(i).Path.size() <= bestPath.size()) {
+                    numBest++;
+                }
+                if(colony.get(i).Path.size() >= worstsize) {
+                    worstsize = colony.get(i).Path.size();
+                    numWorst++;
+                }
+                averageLength += colony.get(i).Path.size();
+            }
+            averageLength = averageLength/colony.size();
+
+            //Output final results
+            System.out.println();
+            System.out.println("Final Pheremone strengths: ");
+            System.out.println(pheremones);
+            System.out.println("Degrading by: " + degrade);
+            System.out.println();
+            System.out.println("Best Path:");
+            System.out.println("(First instance of shortest path - other equally short paths possible)");
+            System.out.println("   Ant " + (bi + 1) + " " + bestPath.toString());
+            for (int i = 0; i < bestPath.size();i++){
+                System.out.print("   " + sets.get(bestPath.get(i)));
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println("General Statistics:");
+            System.out.println("   Length of Best Path: " + bestPath.size());
+            System.out.println("   Number of times best path achieved: " + numBest);
+            System.out.println("   Length of worst path: " + worstsize);
+            System.out.println("   Number of times worst path seen: " + numWorst);
+            System.out.println("   Average path length: " + averageLength);
+
+
         }catch (Exception e){
-            System.out.println("Arguments not in correct format");
-            System.out.println("Try: FileName.txt");
+            System.out.println("ERROR in Main: " + e.getMessage());
         }
 
     }
 
-// Reads in the problem Instance to be dealt with
+    // Reads in the problem Instance to be dealt with
     public static void getProblem(String filename){
         String line = "";
         try {
@@ -57,6 +118,7 @@ public class SCP {
             for (int i = 0; i < inString.length; i++) {
                 universe.add(Integer.parseInt(inString[i]));
             }
+            Collections.sort(universe);
             System.out.println("Universe:" + universe);
 
             //Gather in the sets
@@ -70,8 +132,17 @@ public class SCP {
                 sets.add(set);
                 set = new ArrayList<Integer>();
                 line = reader.readLine();
+
+                //Create a blank pheremone
+                pheremones.add(0);
             }
             System.out.println("Sets: " + sets);
+
+            //Create the pheremones
+            System.out.println("PheremoneSize: " + pheremones.size());
+
+            //Set the degradation
+            degrade = generationSize;
 
         }catch(FileNotFoundException e){System.out.println("File Not Found : " + filename);}
         catch(Exception e){
@@ -85,26 +156,16 @@ public class SCP {
         }
     }
 
-// Creates the colony and first generation of Ants
+    // Creates the colony and first generation of Ants
     public static void createColony(){
         try {
             //Figure out how big the colony and each generation should be - relative to the number of sets in the search space
             // Naieve - one ant for every set in sets, one generation
-            colonySize = sets.size();
-            generationSize = sets.size();
-
+            
             //Create the colony
-            while (colony.size() <= colonySize) {
+            while (colony.size() < colonySize) {
                 colony.add(new Ant());
             }
-
-
-            //Send out the first generation (gen 0) 1 step
-            for (Ant ant : colony) {
-                ant.addToPath(ant.getNextStep(sets));
-            }
-
-            //printColony();
 
         }catch(Exception e){
             System.out.println("ERROR in createColony");
@@ -114,19 +175,50 @@ public class SCP {
         }
     }
 
-// Runs a generation through till there are no places that each hasn't visited
+    // Runs the current generation through
     public static void runGeneration(){
-        for (Ant ant: colony) {
-            while (ant.seenAll == false){
-                ant.addToPath(ant.getNextStep(sets));
+        int adjust = generationSize*generation;
+         //Send out the ants to their start points
+         for (int i = generationSize -1; i>0; i--) {
+            colony.get(i + adjust).startPositions(sets);
+        }
+
+        //move the ants out until they have found a valid solution or seen all nodes
+        for (int i = 0; i<generationSize;i++) {
+            while (colony.get(i + adjust).seenAll == false && colony.get(i + adjust).seenAllNumbers(universe, sets) == false){
+                //System.out.println("Ant " + (i + adjust));
+                colony.get(i + adjust).addToPath(colony.get(i + adjust).getNextStep(sets, pheremones));
             }
         }
+
+        //Create the pheremone trail
+        for (Ant a:colony) {
+            for (Integer i: a.Path) {
+                pheremones.set(i, pheremones.get(i) + 1);
+            }
+        }
+
+        generation++;
+        System.out.println("Generation: " + generation);
+
+        //Degrade the pheremone trail if applicable
+        System.out.println("Pheremones: " + pheremones);
+        if(generation > 1){
+            for (int i = 0; i < pheremones.size(); i++){
+                pheremones.set(i, pheremones.get(i) - degrade);
+                if (pheremones.get(i) < 0) pheremones.set(i, 0);
+            }
+        }
+        System.out.println("Pheremones: " + pheremones);
+
+        //printColony();
+//        System.out.println("Adjust by " + adjust);
     }
 
-//Auxillary method to output the colony
+    //Auxillary method to output the colony
     public static void printColony(){
         for (int i = 0; i < colony.size(); i++) {
-            System.out.println("Ant " + i  + " has path " + colony.get(i).toString());
+            System.out.println("Ant " + (i + 1) + " " + colony.get(i).toString());
         }
     }
 }
